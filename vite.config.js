c5b21be0cb0a7fee04998f448f1f8755e8e8a66c
facundo.ramirez <facundo.ramirez@pgtech.ar>
2025-11-21 03:08:08 -0300

    Fix vite config and rebuild for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue'; 
-import { fileURLToPath, URL } from 'node:url'; // Módulos de ruta para el alias
-
-// Usamos './' para forzar rutas relativas (solución para GitHub Pages)
-const REPO_BASE = './'; 
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  // 1. Establecer la base de las rutas a './' (relativo)
- 
-  base: REPO_BASE, 
-  // 2. Activar el plugin de Vue
+  base: '/', 
   plugins: [vue()], 
-
-  // 3. Configuración de Resolución de Alias (para que @/ funcione)
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  
-  // 4. Configuración CRÍTICA del build: Aplanamiento de la estructura
-  build: {
-    outDir: 'dist', 
-    rollupOptions: {
-      output: {
-        // Establecer nombres de archivo para aplanar la estructura
-        // Los hashes seguirán existiendo, pero estarán directamente en 'dist/'
-        assetFileNames: '[name]-[hash].[ext]', 
-        chunkFileNames: '[name]-[hash].js',
-        entryFileNames: '[name]-[hash].js' 
-      }
     }
   }
 });
