0c61c185ff0cc6a916c54c018b2dd378b7c058e6
facundo.ramirez <facundo.ramirez@pgtech.ar>
2025-11-21 04:21:29 -0300

    sdfsd

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,15 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: '/nexovant/',
+  base: "/nexovant/",
 
   plugins: [vue()],
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        assetFileNames: `assets/[name][extname]`, // 👈 NO HASH
-        chunkFileNames: `assets/[name].js`,
-        entryFileNames: `assets/[name].js`,
-      }
-    }
-  }
 });
