6f5a1ed6a47704f1d0da4868f77e47552af23a2c
Facundo-PG <facundo.ramirez@pgtech.ar>
2026-09-04 22:50:39 -0300

    asdasd

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,7 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      /*"@": fileURLToPath(new URL("./src", import.meta.url)),*/
-      "@": path.resolve(__dirname, './src'),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
