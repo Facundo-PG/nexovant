4d497c318b9d51b84013f2160a015f934d37d92a
Facundo-PG <facundo.ramirez@pgtech.ar>
2026-09-04 22:43:58 -0300

    asdasd

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import path from "path";
 
 export default defineConfig({
   base: "/",
-  build: { outDir: 'docs' },
+  build: { outDir: 'docs', emptyOutDir: true, },
   plugins: [vue()],
 
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      /*"@": fileURLToPath(new URL("./src", import.meta.url)),*/
+      "@": path.resolve(__dirname, './src'),
     },
   },
 });
